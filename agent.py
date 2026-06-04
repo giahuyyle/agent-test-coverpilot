2080c6b6c2a360db67802d7160382e3608ad984a
Gia Huy Le <huyyyylegia@gmail.com>
2026-06-04 13:54:14 -0600

    chore: simple agent

---
+++ agent.py
@@ -0,0 +1,19 @@
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="openai:gpt-5.4",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
+)
+print(result["messages"][-1].content_blocks)
